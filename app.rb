41cdc39630a5767049038b4771c31e8c09dde690
Dagny-Rebhan <drebhan@buffaloseminary.org>
2018-08-03 15:18:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,53 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+get "/" do
+  	erb :quiz
+  end
+  
+   get "/home" do
+  	erb :quiz
+  end
+  
+  get "/about" do 
+    erb :about
+  end
+  
+  get '/nyc' do
+    erb :nyc
+  end
+  
+   get '/paris' do
+    erb :paris
+  end
+  
+   get '/tokyo' do
+    erb :tokyo
+  end
+  
+   get '/sydney' do
+    erb :sydney
+  end
+  
+   get '/cape_town' do
+    erb :cape_town
+  end
+
+  post "/results" do
+    city = choose_city(params)
+    puts city
+    puts params.to_s
+    if city == "NYC"
+      erb :nyc
+    elsif city == "Tokyo"
+      erb :tokyo 
+    elsif city == "Paris"
+      erb :paris 
+    elsif city == "Sydney"
+      erb :sydney
+    else city == "Cape Town"
+      erb :cape_town
+    end 
+  end
+
 end
